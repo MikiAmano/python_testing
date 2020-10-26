995bafa7908410595a6b56c5f6410a1cbd79f251
MikiAmano <yulia404@yandex.ru>
2020-10-26 23:08:11 +0500

    рефакторинг, вынесла параметры в отдельный класс

--- group.py
+++ group.py
@@ -5,3 +5,10 @@ class Group:
         self.name = name
         self.header = header
         self.footer = footer
+
+class Contact:
+
+    def __init__(self, firstname, lastname, phone):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.phone = phone
--- test_addContact.py
+++ test_addContact.py
@@ -1,39 +1,71 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
-from group import Group
+from group import Contact
+
 
 class TestAddContact(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(30)
 
-    def test_untitled_test_case(self):
-        wd = self.wd
+    def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
+
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_new_contact_page(self, wd):
         wd.find_element_by_link_text("add new").click()
+
+    def create_contact(self, wd, group):
+        # init contact creation
         wd.find_element_by_name("firstname").click()
+        # fill contact form
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("Maria")
+        wd.find_element_by_name("firstname").send_keys(group.firstname)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("Ivanova")
+        wd.find_element_by_name("lastname").send_keys(group.lastname)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("837948273948")
+        wd.find_element_by_name("home").send_keys(group.phone)
+        # submit contact creation
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+
+    def return_to_home_page(self, wd):
         wd.find_element_by_link_text("home page").click()
+
+    def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
+
+    def test_add_contact(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_new_contact_page(wd)
+        self.create_contact(wd, Contact(firstname="Maria", lastname="Ivanova", phone="837948273948"))
+        self.return_to_home_page(wd)
+        self.logout(wd)
+
+    def test_add_empty_contact(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_new_contact_page(wd)
+        self.create_contact(wd, Contact(firstname="", lastname="", phone=""))
+        self.return_to_home_page(wd)
+        self.logout(wd)
 
     def tearDown(self):
         self.wd.quit()
+
 
 if __name__ == "__main__":
     unittest.main()
