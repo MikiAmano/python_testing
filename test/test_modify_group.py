7608b07985d68c9aed58e64cc5c538e81a72591e
MikiAmano <yulia404@yandex.ru>
2020-11-08 21:22:11 +0500

    Изменения способа модификации

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 
-def test_modify_first_group(app):
+def test_modify_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify_first_group(Group(name="aaaa", header="bbbb", footer="cccc"))
+    app.group.modify_first_group(Group(name="new group"))
     app.session.logout()
+
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="new header"))
+    app.session.logout()
+
